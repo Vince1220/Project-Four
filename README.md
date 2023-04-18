a6596eca46fd643dd9a8c834127b598b311be9c2
Vince1220 <guitarvince025@yahoo.com>
2023-04-18 14:46:11 -0400

    final revision

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Project-Four
+-- What do you think makes a successful AirBnB rental in this market? What factors seem to be at play the most?
+-- Write a few sentences and include them with your project submission in the README file 
+From the most reviewed locations customer choice determines success in the market with the most stays and reviews.
+Price and length of stay are also factors are at play. Also if the reviews show a percentage of rental activity, the most reviewed locations gets the most new renters.
--- sql_in_action-starter.sql
+++ sql_in_action-starter.sql
@@ -3,27 +3,30 @@
 --    NOTE: When writing queries, make sure each one ends with a semi-colon
 
 -- SELECT * FROM final_airbnb;
+select * from final_airbnb;
 
 
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 1 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find out how many rows are in the table "final_airbnb"
 -- EXPECTED OUTPUT: 146
-
+select count(id) from final_airbnb;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 2 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find out the name of the host for "host_id" 63613
 -- HINT: "Where" could it be?
 
 -- EXPECTED OUTPUT: Patricia
-
+select host_id, host_name
+from final_airbnb
+where host_id = 63613;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 3 >>>>>>>>>>>>>>>>>>>>>>>
 -- Query the data to just show the unique neighbourhoods listed
 -- HINT: This is a "distinct" operation...
 
 -- EXPECTED OUTPUT: 40 neighbourhoods listed
-
+select distinct neighbourhood from final_airbnb;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 4 >>>>>>>>>>>>>>>>>>>>>>>
 
@@ -33,21 +36,24 @@
 -- FOOD FOR THOUGHT: Think about the results. Are the high and low prices outliers in this data set?
 
 -- EXPECTED OUTPUT: Highest = 785, Lowest = 55
-
+select Min(price), Max(price)
+from final_airbnb;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 5 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find the average availability for all listings in the data set (using the availability_365 column)
 -- HINT: Aggregates are more than just big rocks...
 
 -- EXPECTED OUTPUT: 165.3904
-
+select avg(availibility_365) from final_airbnb;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 6 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find all listings that do NOT have a review
 -- HINT: There are a few ways to go about this. Remember that an empty cell is "no value", but not necessarily NULL
 
 -- EXPECTED OUTPUT: 6 rows
-
+select *
+from final_airbnb
+where number_of_reviews = 0;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 7 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find the id of the listing with a room_type of "Private room" that has the most reviews 
@@ -55,6 +61,11 @@
 
 -- EXPECTED OUTPUT: 58059
 
+Select id, room_type, number_of_reviews
+from final_airbnb
+where room_type = "private room"
+order by number_of_reviews desc
+limit 1;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 8 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find the most popular neighbourhood for listings 
@@ -63,13 +74,21 @@
 
 -- EXPECTED OUTPUT: Williamsburg
 -- INVESTIGATE: Should Williamsburg be crowned the most popular neighbourhood?
-
+select neighbourhood
+from final_airbnb
+group by neighbourhood
+order by count(id)desc
+limit 1;
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 9 >>>>>>>>>>>>>>>>>>>>>>>
 -- Query the data to discover which listing is the most popular using the reviews_per_month for all listings with a minimum_nights value of less than 7
 -- HINT: Sorting is still your friend! So are constraints.
 
 -- EXPECTED OUTPUT: 58059
-
+select *
+from final_airbnb
+where minimum_nights < 7
+order by number_of_reviews desc
+limit 1;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 10 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find out which host has the most listings. 
@@ -78,7 +97,11 @@
 -- HINT: Work this one step at a time. See if you can find a way to just display the count of listings per host first.
 
 -- EXPECTED OUTPUT: The Box House Hotel with 6 listings
-
+select host_name, count(id) as calculated_number_of_listings
+from final_airbnb
+group by host_name
+order by calculated_number_of_listings desc
+limit 1;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 11 >>>>>>>>>>>>>>>>>>>>>>>
 -- <<<<<<<<<<<<<<<<<<<<<<< WRAP UP >>>>>>>>>>>>>>>>>>>>>>>>>
@@ -89,4 +112,6 @@
 -- <<<<<<<<<<<<<<<<<<<<< ** BONUS ** >>>>>>>>>>>>>>>>>>>>>>>
 -- Find the the percent above or below each listing is compared to the average price for all listings.
 -- HINT: No hints! It's a bonus for a reason :)
-
+select id, price, avg(price), price / avg(price) * 100-100 percent_above_or_below_avg_price
+from final_airbnb
+order by percent_above_or_below_average_price desc;
